1407f8d17e2825634ad8e1e6230ea1214148ffb7
M Mantle <mmantlehud@googlemail.com>
2021-11-01 10:51:30 +0000

    Initial commit

---
+++ js/countries.js
@@ -0,0 +1,67 @@
+function loadData(url,callback)
+{
+	fetch(url).then(function(response) {
+		return response.json();
+	}).then(function(json) {
+		callback(json)
+	});
+} //end of loadData()
+
+//declare globally
+//these variables need to be accessed by several different functions
+
+let listDiv; //the <div> that will hold the list of countries
+let detailsDiv; //the <div> that will hold the details elements
+let titleEl;
+let capitalEl;
+let populationEl;
+
+function createHandler(country)
+{
+	//this function uses a closure to associate data with a function
+	return function(){
+		titleEl.textContent = country.name;
+		capitalEl.textContent = country.capital;
+		populationEl.textContent = country.population;
+		listDiv.classList.add("hide");
+		detailsDiv.classList.remove("hide");
+	}
+} 
+
+function populateList(countries)
+{
+	const countriesFragment = document.createDocumentFragment();
+	countries.forEach(function(country){
+		const newLi=document.createElement("li");
+		newLi.textContent=country.name;
+		newLi.addEventListener("click", createHandler(country), false)
+		countriesFragment.appendChild(newLi);
+	})
+	const countriesList=document.querySelector("#countries-list");
+	countriesList.appendChild(countriesFragment);
+}
+
+function goBack(){
+	listDiv.classList.remove("hide");
+	detailsDiv.classList.add("hide");
+}
+
+function init(){
+	//grab hold of HTML elements
+	listDiv =  document.querySelector("#list");
+	detailsDiv =  document.querySelector("#details");
+	titleEl = document.querySelector("#title");
+	capitalEl = document.querySelector("#capital");
+	populationEl = document.querySelector("#population");
+	backBtn = document.querySelector("#backBtn");
+
+	//add event listener for the back button
+	backBtn.addEventListener("click",goBack,false);
+
+	//hide the details view on page load
+	detailsDiv.classList.add("hide");
+	//make fetch request
+	loadData("data/countries.json",populateList);
+}
+
+init();
