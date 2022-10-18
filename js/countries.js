1e4d9a24faff7d578ad5b322f39c5a4e842736fd
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-10-18 13:56:14 +0100

    Update countries.js

--- js/countries.js
+++ js/countries.js
@@ -3,7 +3,7 @@ function loadData(url,callback)
 	fetch(url).then(function(response) {
 		return response.json();
 	}).then(function(json) {
-		callback(json)
+		callback(json); //this calls populateList(), it's a callback function
 	});
 } //end of loadData()
 
